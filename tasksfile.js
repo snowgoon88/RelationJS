69bb6a44a72188b0d4998a3d7410ea7e4f7bb76c
Alain Dutech <snowgoon88@gmail.com>
2020-08-08 10:21:06 +0200

    UPD todo and task documentation

--- tasksfile.js
+++ tasksfile.js
@@ -3,14 +3,18 @@
 //
 // see https://github.com/pawelgalazka/tasksfile
 
-const { sh, cli } = require('tasksfile')
+const { sh, cli, help } = require('tasksfile')
 
 function gtags( options ) {
     sh( 'find src -type f -print > gtags.files && gtags' )
 }
+help( gtags, 'Regenerate GTAGS')
+
 function cleantags( option ) {
     sh( 'rm GTAGS GPATH GRTAGS gtags.files' )
 }
+help( cleantags, 'Remove GTAGS files etc...' )
+
 function hello(options, name = 'Mysterious') {
   console.log(`Hello ${name}!`)
 }
