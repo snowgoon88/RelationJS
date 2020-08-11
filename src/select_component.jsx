895e1434c7c71c3601b2192e9057ba8d4e901873
Alain Dutech <snowgoon88@gmail.com>
2020-08-11 19:14:33 +0200

    ADD submit, cleanup

--- src/select_component.jsx
+++ src/select_component.jsx
@@ -8,23 +8,23 @@
 const SelectC = (props) => {
   console.log( "SelectC.create" );
   const [value, setValue] = React.useState( props.value );
-  const [tab, setTab] = React.useState( ['A', 'B'] );
   const [hiddenPopup, setHiddenPopup] = React.useState( true );
   const [options, setOptions] = React.useState( props.options );
   const [idxChoice, setIdxChoice] = React.useState( 0 );
 
   // reference to input Element, through each instance of Component
-  // easier to get value
-  //const inputRef = React.useRef();
+  // easier to get value, NEED ref={inputRef} in input DOM
+  // TODO usefull 
+  const inputRef = React.useRef();
 
-  const handleChange = (event) => {
-    console.log( "SelectC.handleChange", event.target.value );
-  }
+  /* /*const handleChange = (event) => {
+   *   console.log( "SelectC.handleChange", event.target.value );
+   * }/ */
   const handleChangeText = (event) => {
     console.log( "SelectC.handleChangeText", event.target.value );
     setValue( event.target.value );
   }
-
+  
   const handleKeyPress = (keyEvent) => {
     console.log( "SelectC.kPress", keyEvent.key, keyEvent.code, keyEvent.keyCode );
   }
@@ -33,14 +33,14 @@ const SelectC = (props) => {
 
     // move selection idx up or down if visible
     if (hiddenPopup == false) 
-    if( keyEvent.key == "ArrowDown") {
-      if( idxChoice < (options.length - 1)) {
-        setIdxChoice( idxChoice => idxChoice + 1 );
+      if( keyEvent.key == "ArrowDown") {
+        if( idxChoice < (options.length - 1)) {
+          setIdxChoice( idxChoice => idxChoice + 1 );
+        }
+        else {
+          setIdxChoice( 0 );
+        }
       }
-      else {
-        setIdxChoice( 0 );
-      }
-    }
     else if( keyEvent.key == "ArrowUp") {
       if( idxChoice > 0 ) {
         setIdxChoice( idxChoice => idxChoice - 1 );
@@ -57,12 +57,37 @@ const SelectC = (props) => {
     // search pattern, case insensitive
     if( inputEvent.target.value.length > 0 ) {
       let pattern = RegExp( regExpEscape(inputEvent.target.value), "i" );
-    
-      setOptions( props.options.filter( (item) => pattern.test( item ) ));
+      let tmpOptions = props.options.filter( (item) => pattern.test( item ) );
+      if( tmpOptions.length > 0 ) {
+        setOptions( props.options.filter( (item) => pattern.test( item ) ));
+        setIdxChoice( 0 );
+      }
+      else {
+        setOptions( ["Choix impossible"] );
+        setIdxChoice( -1 );
+      }
     }
     else {
       setOptions( props.options );
     }
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log( "SelectC.submit" );
+    if( idxChoice >= 0 ) {
+      console.log( "  valid=", options[idxChoice] );
+      setValue( options[idxChoice] );
+      setHiddenPopup( true );
+    }
+    else {
+      alert( "Invalid choice" );
+    }
+  }
+  const handleCleanup = (event) => {
+    setValue( "" );
+    setOptions( props.options );
+    setIdxChoice( 0 );
+    setHiddenPopup( true );
   }
   
   const listOptions = options.map( (item,index) =>
@@ -72,89 +97,100 @@ const SelectC = (props) => {
       {item}
     </li>
   );
+  const clearIcon =
+    <div onClick={handleCleanup} className="icon_container">
+      <svg height="20" width="20" viewBox="0 0 20 20" focusable="false"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg>
+    </div>;
 
-  const handleBtnHide = (event) => {
-    // use updator to take into account old value of hiddenPopup
-    setHiddenPopup( hiddenPopup => !hiddenPopup );
-  }
+  /* const handleBtnHide = (event) => {
+   *   // use updator to take into account old value of hiddenPopup
+   *   setHiddenPopup( hiddenPopup => !hiddenPopup );
+   * } */
   const handleInputFocus = (event) => {
     console.log( "SelectC.handleInputFocus" );
     setHiddenPopup( false );
   }
-  const handleBtnA = (event) => {
-    // add if missing, otherwise remove
-    // using operation that create new array, to trigger re-render
-    if (tab.find( item => item == 'A' )) {
-      setTab( tab.filter( item => item != 'A' ));
-    }
-    else {
-      setTab( [...tab, 'A'] );
-    }
-  }
-  const handleBtnB = (event) => {
-    // add if missing, otherwise remove
-    // using operation that create new array, to trigger re-render
-    if (tab.find( item => item == 'B' )) {
-      setTab( tab.filter( item => item != 'B' ));
-    }
-    else {
-      setTab( [...tab, 'B'] );
-    }
-  }
-  const handleBtnC = (event) => {
-    console.log( "Select.handleBtnC tab=",tab );
-    // add if missing, otherwise remove
-    // using operation that create new array, to trigger re-render
-    if (tab.find( item => item == 'C' )) {
-      setTab( tab.filter( item => item != 'C' ));
-    }
-    else {
-      setTab( [...tab, 'C'] );
-    }
-     
-        
-    // need to copy on tab because hase to change tab
-    //setTab( [ ...tab, 'C'] );
-    /* let nTab = tab;
-     * nTab.push( 'V' );
-     * console.log( "  newTab=",nTab, (tab == nTab), (tab === nTab), (nTab == nTab.slice()), (nTab === nTab.slice()) );
-     * setTab( nTab.slice() ); */
-    //setTab( tab => tab.push( 'C' ) );
-  }
+  /* const handleBtnA = (event) => {
+   *   // add if missing, otherwise remove
+   *   // using operation that create new array, to trigger re-render
+   *   if (tab.find( item => item == 'A' )) {
+   *     setTab( tab.filter( item => item != 'A' ));
+   *   }
+   *   else {
+   *     setTab( [...tab, 'A'] );
+   *   }
+   * }
+   * const handleBtnB = (event) => {
+   *   // add if missing, otherwise remove
+   *   // using operation that create new array, to trigger re-render
+   *   if (tab.find( item => item == 'B' )) {
+   *     setTab( tab.filter( item => item != 'B' ));
+   *   }
+   *   else {
+   *     setTab( [...tab, 'B'] );
+   *   }
+   * }
+   * const handleBtnC = (event) => {
+   *   console.log( "Select.handleBtnC tab=",tab );
+   *   // add if missing, otherwise remove
+   *   // using operation that create new array, to trigger re-render
+   *   if (tab.find( item => item == 'C' )) {
+   *     setTab( tab.filter( item => item != 'C' ));
+   *   }
+   *   else {
+   *     setTab( [...tab, 'C'] );
+   *   }
+   *    
+   *  */      
+  // need to copy on tab because hase to change tab
+  //setTab( [ ...tab, 'C'] );
+  /* let nTab = tab;
+   * nTab.push( 'V' );
+   * console.log( "  newTab=",nTab, (tab == nTab), (tab === nTab), (nTab == nTab.slice()), (nTab === nTab.slice()) );
+   * setTab( nTab.slice() ); */
+  //setTab( tab => tab.push( 'C' ) );
+  //}
   
   // render
   return (
     <div
-      onKeyPress={handleKeyPress}
+      //onKeyPress={handleKeyPress}
       onKeyUp={handleKeyUp}
-      onInput={handleInput}
+      //onInput={handleInput}
     >
-      <div>
-        <button onClick={handleBtnHide}>Hide/show</button>
-      </div>
-      <div>
-        Truc
-        <input
-          type="text"
-          value={value}
-          onChange={handleChangeText}
-          onFocus={handleInputFocus}
-        />
-        <button>V</button>
-      </div>
-      <div className={`popup ${hiddenPopup ? "hidden" : ""}`}>
-        <ul className="dropdown">
-          {listOptions}
-        </ul>
-      </div>
-      <div>
-        <button onClick={handleBtnA}>A</button>
-        <button onClick={handleBtnB}>B</button>
-        <button onClick={handleBtnC}>C</button>
-      </div>
-      <div>
-        tab=[{tab.toString()}]
-      </div>
+      {/* <div>
+          <button onClick={handleBtnHide}>Hide/show</button>
+          </div> */}
+      <form
+        onSubmit={handleSubmit}
+      >
+        <div>
+          Truc
+          <input
+          //ref={inputRef}
+            type="text"
+            value={value}
+            onInput={handleInput}
+            onChange={handleChangeText}
+            onFocus={handleInputFocus}
+          />
+          {clearIcon}
+          <button>V</button>
+        </div>
+        <div className={`popup ${hiddenPopup ? "hidden" : ""}`}>
+          <ul className="dropdown">
+            {listOptions}
+          </ul>
+        </div>
+      </form>
+      {/* <div>
+          <button onClick={handleBtnA}>A</button>
+          <button onClick={handleBtnB}>B</button>
+          <button onClick={handleBtnC}>C</button>
+          </div>
+          <div>
+          tab=[{tab.toString()}]
+          </div> */}
     </div>
   );
 }
@@ -163,7 +199,7 @@ const SelectC = (props) => {
 // ******************************************************************* RelationC
 // Escape Regex patterns
 function regExpEscape(s) {
-    return s.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
+  return s.replace(/[-\\^$*+?.()|[\]{}]/g, "\\$&");
 }
 
 // *****************************************************************************
@@ -180,3 +216,4 @@ ReactDOM.render(
 );
 
 
+/* <svg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-6q0nyr-Svg"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg> */
